f15a9f0d1c08f4f9b04f0d760f7b3ec1e5e19a84
AMITABH PANDEY <amitabhp96@gmail.com>
2025-08-19 23:54:28 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,5 +2,6 @@ class Simple{
     public static void main(String args[]){  
      System.out.println("Hello batch40 team My Name is  Amitabh Pandey ");
       System.out.println("Hello Batch batch40 WITH FOR STUDENTS RUNNING in front of Amitabh in making CI automated vikas teach devops CI proecss");
+        System.out.println("Hello Batch batch40 i have complete Git and github");
     }  
 } 
